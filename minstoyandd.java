58726bde772c6b3e452758ee48995d04404f1ad9
ManasaY13 <64654749+ManasaY13@users.noreply.github.com>
2020-05-03 13:10:34 +0530

    Update minstoyandd.java
    Input minutes. Outputs No. of years plus days.

--- minstoyandd.java
+++ minstoyandd.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 class MinstoDY{
     public static void main(String []args){
         Scanner scan=new Scanner(System.in);
-        int x=0,y=0,z=0;
+        int x=0,y1=0,y2=0,z=0,w=0;
         System.out.println("Input the number of minutes: ");
         x=scan.nextInt();
-        y=x/1440;
         z=x/(365*1440);
-        System.out.println(x+" minutes is approximately "+ z +" years and "+ y + " days ");
+        y1=x/1440;
+        y2=365*z;
+        w=y1-y2;
+        
+        System.out.println(x+" minutes is approximately "+ z +" years and "+ w + " days ");
+
+
+
     }
 }
